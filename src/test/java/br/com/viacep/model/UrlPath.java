39fb84a1b6418faff0b35dfd9fad9a18f20cf6d7
AdrianoAGuterres <adrianog@dbserver.com.br>
2021-04-11 20:52:18 -0300

    Criação do environment.properties

--- src/test/java/br/com/viacep/model/UrlPath.java
+++
@@ -1,7 +0,0 @@
-package br.com.viacep.model;
-
-public interface UrlPath {
-    String BASE_URI = "https://viacep.com.br/";
-    String CEP_PATH = "ws/{CEP}/json/";
-    String ENDERECO_PATH = "ws/{UF}/{CIDADE}/{LOGRADOURO}/json/";
-}
